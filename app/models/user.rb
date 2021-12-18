f61b76c7bdf45f12606ec255c08d52462ce4b33f
itosyo-takumi <itosyo4126@gmail.com>
2021-12-18 17:00:58 +0900

    ユーザー管理機能

---
+++ app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX, message: 'Password is invalid. Include both letters and numbers'
+       
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々]+\z/, message: 'family_name is invalid. Input full-width characters' } do
+           validates :family_name
+           validates :first_name
+         end
+       
+         with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'Last name kana is invalid. Input full-width katakana characters' } do
+           validates :family_name_kana
+           validates :first_name_kana
+         end
+
+     with_options presence: true do
+      validates :nickname
+      validates :family_name
+      validates :first_name
+      validates :family_name_kana
+      validates :first_name_kana
+      validates :birth_day
+   end     
+
+
+
+
+
+end
