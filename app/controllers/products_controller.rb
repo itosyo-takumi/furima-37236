6e98ff4c9d612eb3986718ab8186fec33d671b09
itosyo-takumi <itosyo4126@gmail.com>
2021-12-25 12:46:43 +0900

    商品情報編集機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :move_to_index, only: [:edit]
+
   def index
     @products = Product.includes(:user).order('created_at DESC')
   end
@@ -10,8 +12,7 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    if @product.valid?
-      @product.save
+    if @product.save
       redirect_to root_path
     else
       render :new
@@ -22,10 +23,31 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path
+    else
+      render :edit
+    end
+  end
+
+
   private
 
   def product_params
     params.require(:product).permit(:name, :price, :description, :status_id, :shipping_cost_id, :shipping_days_id,
                                     :prefecture_id, :category_id, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @product = Product.find(params[:id])
+    if @product.user_id != current_user.id
+       redirect_to action: :index
+    end  
   end
 end
